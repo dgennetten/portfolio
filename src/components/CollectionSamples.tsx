ae7ba73a8e42e802a4c0c2e3be53ccc350ffe6f5
Douglas Gennetten <douglas@gennetten.com>
2025-04-27 15:29:42 -0600

    Home page update

--- src/components/CollectionSamples.tsx
+++ src/components/CollectionSamples.tsx
@@ -28,37 +28,43 @@ const CollectionSamples: React.FC = () => {
           </div>
         ))}
       </div>
-      <div className="flex space-x-4 items-center justify-center mt-12 py-8">
-        <Link 
-          to="/gallery/drawings"
-          className="inline-block px-6 py-3 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
-        >
-          Drawings 
-        </Link>
-        <Link 
-          to="/gallery/paintings"
-          className="inline-block px-6 py-3 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
-        >
-          Paintings 
-        </Link>
-       <Link 
-          to="/gallery/sculptures"
-          className="inline-block px-6 py-3 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
-        >
-          Sculpture 
-        </Link>
-        <Link 
-          to="/gallery/geometric"
-          className="inline-block px-6 py-3 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
-        >
-          Geometric 
-        </Link>
-         <Link 
-          to="/gallery/photography"
-          className="inline-block px-6 py-3 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
-        >
-          Photography 
-        </Link>
+      <div className="container mx-auto px-4 ">
+        
+        <div className="flex space-x-4 items-center justify-center mt-12 py-0">
+          <h4 className="text-3xl md:text-4xl font-light text-center mb-0">All Collections</h4>
+          </div>
+        <div className="flex space-x-4 items-center justify-center mt-12 py-0">
+          <Link
+            to="/gallery/drawings"
+            className="inline-block px-6 py-0 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
+          >
+            Drawings
+          </Link>
+          <Link
+            to="/gallery/paintings"
+            className="inline-block px-6 py-0 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
+          >
+            Paintings
+          </Link>
+          <Link
+            to="/gallery/sculptures"
+            className="inline-block px-6 py-0 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
+          >
+            Sculpture
+          </Link>
+          <Link
+            to="/gallery/geometric"
+            className="inline-block px-6 py-0 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
+          >
+            Geometric
+          </Link>
+          <Link
+            to="/gallery/photography"
+            className="inline-block px-6 py-0 border border-gray-800 text-gray-800 hover:bg-gray-800 hover:text-white transition-all duration-300"
+          >
+            Photography
+          </Link>
+        </div>
       </div>
     </div>
   );
