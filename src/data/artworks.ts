f201559c8e316c52e5c587c27fff8967bdc13c9a
Darren Gennetten <darren@gennetten.com>
2025-06-12 23:17:28 -0400

    added a bunch of photos

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -2,11 +2,20 @@ import { Artwork } from '../types';
 
 export const artworks: Artwork[] = [
   // Logos
-  { id: 'logo-1', title: 'trolley', media: 'vector, 2024', description: 'New trolley logo', imageSrc: '/images/Logos/Trolley.jpg', category: 'logos' },
+  { id: 'logo-1', title: 'Luminary Brewing', media: 'vector, 2013', description: 'New Logo for Luminary Brewing', imageSrc: '/images/Logos/lumbrewlogo.jpg', category: 'logos' },
 
   // Photography
-  { id: 'photo-1', title: 'Untitled', media: 'Digital photography, Olympus prototype, 1999`', description: 'These bonnett and apron decorations are in honor of a still-born or aborted child. Tokyo, Japan.', imageSrc: '/images/Photography/Tokyo10231999.jpg', category: 'photography' },
-
+  { id: 'photo-1', title: 'Rainbo Club', media: 'nikon', description: 'Foggy night outside of the Rainbo Club', imageSrc: '/images/Photography/photography1.jpg', category: 'photography' },
+  { id: 'photo-2', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography2.jpg', category: 'photography' },
+  { id: 'photo-3', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography3.jpg', category: 'photography' },
+  { id: 'photo-4', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography4.jpg', category: 'photography' },
+  { id: 'photo-5', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography5.jpg', category: 'photography' },
+  { id: 'photo-6', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography6.jpg', category: 'photography' },
+  { id: 'photo-7', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography7.jpg', category: 'photography' },    
+  { id: 'photo-8', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography8.jpg', category: 'photography' },
+  { id: 'photo-9', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography9.jpg', category: 'photography' },
+  { id: 'photo-10', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography10.jpg', category: 'photography' },
+  { id: 'photo-11', title: 'Untitled', media: 'nikon', description: 'Some description..', imageSrc: '/images/Photography/photography11.jpg', category: 'photography' },
   // Design Projects 
   { id: 'projectA', title: 'Saxy', media: 'Colorado Yule Marble, 70cm, 2.5cm granite base, 80kg, 2008', description: 'Abstract Saxaphone sculpted from Colorado Yule Marble', imageSrc: '/images/Projects/ProjectA/Saxy0.jpg', category: 'projects' },
   { id: 'projectB', title: 'Project B', media: 'Digital photography, Olympus prototype, 1999`', description: 'These bonnett and apron decorations are in honor of a still-born or aborted child. Tokyo, Japan.', imageSrc: '/images/Projects/ProjectB/MarbleMuse1.jpg', category: 'projects' },
