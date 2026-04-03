6b4f6a16e768c9559e169de00f92e6074f444301
Douglas Gennetten <douglas@gennetten.com>
2026-04-03 14:24:35 -0600

    Update featured artworks in artworks.ts to reflect new project details and replace outdated entries; update Magic Lantern image file.

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -126,7 +126,7 @@ export const artworks: Artwork[] = [
 ];
 
 export const featuredArtworks: Artwork[] = [
-  artworks.find(art => art.id === 'painting-3')!,   // Profile in Guache
+  artworks.find(art => art.id === 'code-2')!,        // Precision Sundial Generator (Code)
   artworks.find(art => art.id === 'sculpture-29')!,  // Final Day at MARBLEmarble36!
   artworks.find(art => art.id === 'photo-4')!,       // Sunrise over Trappers Lake
 ];
